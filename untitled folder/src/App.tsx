0771c385807de8ef52ac23fc8f0b102fa39efb80
Samvel23 <139640934+Samvel23@users.noreply.github.com>
2024-08-27 18:37:15 +0400

    Add files via upload

---
+++ untitled folder/src/App.tsx
@@ -0,0 +1,11 @@
+import { Child } from "./components/Child/Child";
+import { Parent } from "./components/Parent/Parent";
+
+export default function App() {
+  return (
+    <>
+      <Parent />
+      <Child />
+    </>
+  );
+}
